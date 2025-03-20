0fcabcd35c759da4d916892bb78f529e0761e11a
Ayan Koley <koleyayan07@gmail.com>
2025-03-20 11:17:01 +0530

    unauthorized req can also fetched comments

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -9,9 +9,7 @@ import {verifyJwt} from "../middlewares/auth.middlewares.js"
 
 const router = Router();
 
-router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
-
-router.route("/:videoId").get(getVideoComments).post(addComment);
-router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+router.route("/:videoId").get(getVideoComments).post(verifyJwt, addComment);
+router.route("/c/:commentId").delete(verifyJwt, deleteComment).patch(verifyJwt, updateComment);
 
 export default router
